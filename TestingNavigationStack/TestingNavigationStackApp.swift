a3e5438004d4b8680c3c7b661f172cb9dd489e75
Thaís Monteiro <77128802+thaxz@users.noreply.github.com>
2023-10-12 11:32:56 -0300

    first commit

---
+++ TestingNavigationStack/TestingNavigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingNavigationStackApp.swift
+//  TestingNavigationStack
+//
+//  Created by thaxz on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TestingNavigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
