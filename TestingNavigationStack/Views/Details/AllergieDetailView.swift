a4c8650fb9657472b74fc5270f08a6308adde6ad
Thaís Monteiro <77128802+thaxz@users.noreply.github.com>
2023-10-13 12:01:19 -0300

    feature(Router): Testing how to go back to root

--- TestingNavigationStack/Views/Details/AllergieDetailView.swift
+++ TestingNavigationStack/Views/Details/AllergieDetailView.swift
@@ -9,11 +9,16 @@ import SwiftUI
 
 struct AllergiesDetailView: View {
     
+    @EnvironmentObject private var routerManager: NavigationRouter
     let allergies: [Allergie]
     
     var body: some View {
         List {
-
+            Section{
+                Button("back to root"){
+                    routerManager.reset()
+                }
+            }
             Section {
                 ForEach(allergies, id: \.name) { allergy in
                     /*@START_MENU_TOKEN@*/Text(allergy.name)/*@END_MENU_TOKEN@*/
@@ -30,6 +35,7 @@ struct AllergiesDetailView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationStack {
             AllergiesDetailView(allergies: desserts[0].allergies!)
+                .environmentObject(NavigationRouter())
         }
     }
 }
